15f4d1b591a16e2cfd00766125d7a12d8600765f
David Allen <davidallen02@me.com>
2020-10-07 11:36:24 -0700

    Add CBO output gap chart

---
+++ R/cbo-output-gap.R
@@ -0,0 +1,13 @@
+library(magrittr)
+
+dat <- pamngr::get_data("cbopgapp") %>%
+  dplyr::filter(dates >= as.Date("2019-01-01")) %>%
+  tidyr::pivot_longer(cols = -dates, names_to = "variable")
+
+p <- dat %>%
+  pamngr::barplot() %>%
+  pamngr::pam_plot(
+    plot_title = "US Economic Output Gap",
+    plot_subtitle = "CBO Estimate, Percent of GDP",
+    show_legend = FALSE
+  )
